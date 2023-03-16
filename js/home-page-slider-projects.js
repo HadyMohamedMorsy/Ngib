338221e39327b0fed57ae0395d3794b58a6908a2
hady912012@gmail.com <hady912012@gmail.com>
2023-03-16 04:25:14 -0700

    slider-responsive

--- js/home-page-slider-projects.js
+++ js/home-page-slider-projects.js
@@ -5,4 +5,18 @@ const swiperProjects =new Swiper('.swiper-projects', {
     autoplay: {
         delay: 3000,
     },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
   });
