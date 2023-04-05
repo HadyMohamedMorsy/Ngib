effa7454b980065a0e95fafdeb91bbe54d44389a
hady912012@gmail.com <hady912012@gmail.com>
2023-04-06 00:27:19 +0200

    add splash screen

---
+++ js/loading.js
@@ -0,0 +1,19 @@
+const splashScreen = document.getElementById('splash-screen');
+const logoScreen = document.getElementById('logo-screen');
+
+
+fadeIn();
+// Define the fadeIn function
+function fadeIn() {
+    const ScreenLogo = setTimeout(()=>{
+        logoScreen.classList.add('logo-active')
+        clearTimeout(ScreenLogo);
+    }, 2000)
+
+    const ScreenSplash = setTimeout(()=>{
+        splashScreen.classList.add('active');
+        clearTimeout(ScreenSplash);
+    }, 3000)
+
+    
+}
